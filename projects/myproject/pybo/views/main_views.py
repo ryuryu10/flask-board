d5fbb312e39dfd0403c0570392cc8b9cb5bc0624
Seungwoo Yoo <ryuryu10@naver.com>
2021-11-15 18:16:05 +0900

    dev

--- projects/myproject/pybo/views/main_views.py
+++ projects/myproject/pybo/views/main_views.py
@@ -11,3 +11,8 @@ def hello_pybo():
 def index():
     question_list = Question.query.order_by(Question.create_date.desc())
     return render_template('question/question_list.html', question_list=question_list)
+
+@bp.route('/detail/<int:question_id>/')
+def detail(question_id):
+    question = Question.query.get(question_id)
+    return render_template('question/question_detail.html', question=question)
