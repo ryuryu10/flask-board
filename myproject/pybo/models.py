ba78d095027495646df1116607041dd1b3a1bb6d
20527 유승우 <ryuryu103030@gmail.com>
2022-03-14 03:10:42 +0000

    2-05

--- myproject/pybo/models.py
+++ myproject/pybo/models.py
@@ -9,3 +9,4 @@ class Question(db.Model):
 class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'))
+    question = db.relationship('Question', backref=db.backref('answer_set'))
