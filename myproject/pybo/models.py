77ded72cbd4134ab08c16195d0d5b0fb9cb29865
20527 유승우 <ryuryu103030@gmail.com>
2022-03-14 02:38:38 +0000

    models class 생성

---
+++ myproject/pybo/models.py
@@ -0,0 +1,11 @@
+from pybo import db
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text(), nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
+    
+class Answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'))
